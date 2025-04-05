b9a98cc3a15f32f859c94cc126df70b62858f173
Riyad Murad <riyadmurad01@gmail.com>
2025-04-05 23:51:27 +0300

    Testing Dockerhub push

--- imagesElectron/dockerShell.sh
+++ imagesElectron/dockerShell.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-if ! php artisan db:table sessions > /dev/null 2>&1; then
-  echo "Migrating..."
-  php artisan migrate
-fi
+# if ! php artisan db:table sessions > /dev/null 2>&1; then
+#   echo "Migrating..."
+#   php artisan migrate
+# fi
+
+php artisan migrate
 
 php artisan serve --host=0.0.0.0 --port=8000
