417acd88cb759f2582c86a53e0c354ea6eec96c4
Riyad Murad <riyadmurad01@gmail.com>
2025-04-16 17:20:15 +0300

    fixed JWT

--- Electron_Image_Editor/src/renderer/src/Axios/axios.js
+++ Electron_Image_Editor/src/renderer/src/Axios/axios.js
@@ -3,8 +3,8 @@ import axios from 'axios'
 const token = localStorage.getItem("Token");
 
 const axiosBaseUrl = axios.create({
-  // baseURL: "http://15.237.215.230:8000/api/v1",
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL: "http://15.237.215.230:8000/api/v1",
+  // baseURL: "http://localhost:8000/api/v1",
 
   headers: {
     "Content-Type": "application/json",
--- dockerShell.sh
+++ dockerShell.sh
@@ -23,6 +23,8 @@ if [ ! -f .env ]; then
   sed -i "s|DB_DATABASE=.*|DB_DATABASE=${LARAVEL_DATABASE_NAME}|" .env
   sed -i "s|DB_USERNAME=.*|DB_USERNAME=${LARAVEL_DATABASE_USER}|" .env
   sed -i "s|DB_PASSWORD=.*|DB_PASSWORD=${LARAVEL_DATABASE_PASSWORD}|" .env
+  sed -i "s|APP_KEY=.*|APP_KEY=${LARAVEL_APP_KEY}|" .env
+  sed -i "s|JWT_SECRET=.*|JWT_SECRET=${LARAVEL_JWT_SECRET}|" .env
 else
   echo "📄 .env already exists"
 fi
