0ea77669ccd7b6c47acdb56a9888adae6f1795f5
Riyad Murad <riyadmurad01@gmail.com>
2025-04-16 15:57:36 +0300

    table Chats update

--- imagesElectron/database/migrations/2025_04_16_105741_update_chats.php
+++ imagesElectron/database/migrations/2025_04_16_105741_update_chats.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('Chats', function (Blueprint $table) {
+        Schema::table('chats', function (Blueprint $table) {
             $table->text('username')->after('message');
         });
     }
